fdd7b989e62c784b26ddd79bcbc30efbaccc9e1b
TheBatz-115 <ramon.fernandezg7@gmail.com>
2022-12-09 23:28:16 -0700

    Emulator SDK working

--- app/src/main/java/thebatz/ramon/bread/MainActivity.kt
+++ app/src/main/java/thebatz/ramon/bread/MainActivity.kt
@@ -2,11 +2,12 @@ package thebatz.ramon.bread
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.View.INVISIBLE
-import android.view.View.VISIBLE
+import android.view.View.*
+import android.view.WindowManager
 import android.widget.Button
 import android.widget.FrameLayout
 import android.widget.ImageButton
+import androidx.constraintlayout.widget.ConstraintLayout
 
 class MainActivity : AppCompatActivity() {
 
@@ -17,7 +18,7 @@ class MainActivity : AppCompatActivity() {
         val mixingBowlButton = findViewById<ImageButton>(R.id.MixingBowlButton)
 
         // Layouts
-        val bowlContentView = findViewById<FrameLayout>(R.id.BowlContentLayout)
+        val bowlContentView = findViewById<ConstraintLayout>(R.id.BowlContentLayout)
         val BC_BackButton = findViewById<Button>(R.id.BowlContentBackButton)
 
         mixingBowlButton.setOnClickListener {
