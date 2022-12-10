6346b7019e427e0587307f9d449b30c8610c2196
rafe3007 <rafe3007@colorado.edu>
2022-12-09 18:13:10 -0700

    Bowl Content Functionality added

--- app/src/main/java/thebatz/ramon/bread/MainActivity.kt
+++ app/src/main/java/thebatz/ramon/bread/MainActivity.kt
@@ -2,6 +2,11 @@ package thebatz.ramon.bread
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View.INVISIBLE
+import android.view.View.VISIBLE
+import android.widget.Button
+import android.widget.FrameLayout
+import android.widget.ImageButton
 
 class MainActivity : AppCompatActivity() {
 
@@ -9,7 +14,17 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        val mixingBowlButton = findViewById<ImageButton>(R.id.MixingBowlButton)
 
+        // Layouts
+        val bowlContentView = findViewById<FrameLayout>(R.id.BowlContentLayout)
+        val BC_BackButton = findViewById<Button>(R.id.BowlContentBackButton)
 
+        mixingBowlButton.setOnClickListener {
+            bowlContentView.visibility = VISIBLE
+        }
+        BC_BackButton.setOnClickListener {
+            bowlContentView.visibility = INVISIBLE
+        }
     }
 }
